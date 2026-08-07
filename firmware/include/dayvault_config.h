ec70157bf9e9f7bf066000e3a16e6969ca24146a
Ha22yX <kicofy@vip.qq.com>
2026-08-08 01:08:56 +0800

    feat(firmware): vendor FatFs and add SPI1 microSD driver with FatFs glue

--- firmware/include/dayvault_config.h
+++ firmware/include/dayvault_config.h
@@ -7,6 +7,11 @@
 #define PIN_USB_DM        GPIO_PIN_11
 #define PIN_USB_DP        GPIO_PIN_12
 #define PIN_LED           GPIO_PIN_8
+#define PIN_SD_CS        GPIO_PIN_4
+#define PIN_SD_CS_PORT   GPIOA
+#define PIN_SD_SCK       GPIO_PIN_5
+#define PIN_SD_MISO      GPIO_PIN_6
+#define PIN_SD_MOSI      GPIO_PIN_7
 
 #define USB_CDC_RX_LINE_MAX  64u
 
