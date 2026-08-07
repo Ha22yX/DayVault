b311c9c9872f57abe45249003ce64ec02105f321
Ha22yX <kicofy@vip.qq.com>
2026-08-08 01:43:25 +0800

    fix(firmware): make ringbuf update atomic against ISR preemption

--- firmware/src/ringbuf.c
+++ firmware/src/ringbuf.c
@@ -1,5 +1,26 @@
 #include "ringbuf.h"
 
+static unsigned long irq_mask_save(void)
+{
+#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__arm__)
+    unsigned long primask;
+    __asm volatile ("mrs %0, primask" : "=r" (primask) :: "memory");
+    __asm volatile ("cpsid i" ::: "memory");
+    return primask;
+#else
+    return 0;
+#endif
+}
+
+static void irq_mask_restore(unsigned long primask)
+{
+#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__arm__)
+    __asm volatile ("msr primask, %0" :: "r" (primask) : "memory");
+#else
+    (void)primask;
+#endif
+}
+
 void ringbuf_init(ringbuf_t *rb, uint8_t *buf, size_t size)
 {
     rb->buf = buf;
@@ -22,6 +43,7 @@ size_t ringbuf_free(const ringbuf_t *rb)
 size_t ringbuf_write(ringbuf_t *rb, const uint8_t *data, size_t n)
 {
     size_t i;
+    unsigned long pm = irq_mask_save();
     for (i = 0; i < n; i++)
     {
         if (rb->used == rb->size)
@@ -30,17 +52,20 @@ size_t ringbuf_write(ringbuf_t *rb, const uint8_t *data, size_t n)
         rb->head = (rb->head + 1) % rb->size;
         rb->used++;
     }
+    irq_mask_restore(pm);
     return i;
 }
 
 size_t ringbuf_read(ringbuf_t *rb, uint8_t *dst, size_t n)
 {
     size_t i;
+    unsigned long pm = irq_mask_save();
     for (i = 0; i < n && rb->used > 0; i++)
     {
         dst[i] = rb->buf[rb->tail];
         rb->tail = (rb->tail + 1) % rb->size;
         rb->used--;
     }
+    irq_mask_restore(pm);
     return i;
 }
