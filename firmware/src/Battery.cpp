44298b9cc62dcec8583f322c70c50f7b99547718
Ha22yX <kicofy@vip.qq.com>
2026-08-09 18:25:37 +0800

    feat: add Battery module - ADC1 PA0 sense with VREFINT-calibrated VDDA, 8-sample averaged millivolts/percent

---
+++ firmware/src/Battery.cpp
@@ -0,0 +1,72 @@
+#include "Battery.h"
+#include "stm32l4xx_hal.h"
+
+static ADC_HandleTypeDef hadc;
+
+static uint16_t read_adc_channel(uint32_t channel)
+{
+    ADC_ChannelConfTypeDef s = {0};
+    s.Channel = channel;
+    s.Rank = ADC_REGULAR_RANK_1;
+    s.SamplingTime = ADC_SAMPLETIME_247CYCLES_5;
+    HAL_ADC_ConfigChannel(&hadc, &s);
+    HAL_ADC_Start(&hadc);
+    HAL_ADC_PollForConversion(&hadc, 100);
+    uint16_t v = (uint16_t)HAL_ADC_GetValue(&hadc);
+    HAL_ADC_Stop(&hadc);
+    return v;
+}
+
+void bat_init(void)
+{
+    __HAL_RCC_ADC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    GPIO_InitTypeDef g = {0};
+    g.Pin = GPIO_PIN_0;
+    g.Mode = GPIO_MODE_ANALOG;
+    g.Pull = GPIO_PULLDOWN;
+    HAL_GPIO_Init(GPIOA, &g);
+
+    hadc.Instance = ADC1;
+    hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    hadc.Init.Resolution = ADC_RESOLUTION_12B;
+    hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc.Init.ScanConvMode = ADC_SCAN_DISABLE;
+    hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    hadc.Init.LowPowerAutoWait = DISABLE;
+    hadc.Init.ContinuousConvMode = DISABLE;
+    hadc.Init.NbrOfConversion = 1;
+    hadc.Init.DiscontinuousConvMode = DISABLE;
+    hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc.Init.DMAContinuousRequests = DISABLE;
+    hadc.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+    hadc.Init.OversamplingMode = DISABLE;
+    HAL_ADC_Init(&hadc);
+
+    LL_ADC_SetCommonPathInternalCh(__LL_ADC_COMMON_INSTANCE(ADC1), LL_ADC_PATH_INTERNAL_VREFINT);
+
+    HAL_ADCEx_Calibration_Start(&hadc, ADC_SINGLE_ENDED);
+}
+
+uint16_t bat_millivolts(void)
+{
+    uint32_t sum_bat = 0, sum_vrefint = 0;
+    for (int i = 0; i < 8; i++) {
+        sum_bat += (uint32_t)read_adc_channel(ADC_CHANNEL_0);
+        sum_vrefint += (uint32_t)read_adc_channel(ADC_CHANNEL_VREFINT);
+    }
+    uint16_t avg_bat = (uint16_t)(sum_bat / 8u);
+    uint16_t avg_vrefint = (uint16_t)(sum_vrefint / 8u);
+    uint32_t vdda = (uint32_t)(*VREFINT_CAL_ADDR) * 3000UL / avg_vrefint;
+    return (uint16_t)((uint32_t)avg_bat * vdda / 2048u);
+}
+
+uint8_t bat_percent(void)
+{
+    uint16_t mv = bat_millivolts();
+    if (mv <= 3000) return 0;
+    if (mv >= 4200) return 100;
+    return (uint8_t)((mv - 3000u) * 100u / 1200u);
+}
---
+++ firmware/src/Battery.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdint.h>
+void bat_init(void);
+uint16_t bat_millivolts(void);   /* e.g. 3650 = 3.65 V at the battery terminal */
+uint8_t  bat_percent(void);      /* 0..100, linear 3.0 V..4.2 V */
